c63706414d5f886ca64670813dc214622d67c63c
GitHub Action <action@github.com>
2022-04-20 09:42:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/petrmurlykin/vbmigration/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/petrmurlykin/vbmigration/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.petrmurlykin.vbmigration.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.petrmurlykin.vbmigration.MyBundle
 
 class MyApplicationService {
 
